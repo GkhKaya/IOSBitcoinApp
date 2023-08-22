10736e1ff68b3b3990d4516651d9703df265a2b5
GkhKaya <gkhkaya00@gmail.com>
2023-08-22 22:52:49 +0300

    Added design and assets

---
+++ IOSBitcoinApp/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSBitcoinApp
+//
+//  Created by Gokhan Kaya on 22.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
