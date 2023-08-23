b017773e21824bf1ee74b7077b9f8c904752c390
GkhKaya <gkhkaya00@gmail.com>
2023-08-23 21:27:56 +0300

    Made api request and added json parsing

--- IOSBitcoinApp/Model/CoinManager.swift
+++ IOSBitcoinApp/Model/CoinManager.swift
@@ -14,7 +14,44 @@ struct CoinManager {
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency:String){
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
         
+        if let url = URL(string: urlString){
+            let session =  URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url){(data,response,error) in
+                
+                if error != nil{
+                    print(error!)
+                    return
+                }
+                if let safedata = data{
+                    let bitcoinPrice = self.parseJSON(safedata)
+                }
+            }
+            task.resume()
+        }
     }
-    
+
+    func parseJSON(_ data: Data) -> Double? {
+            
+            //Create a JSONDecoder
+            let decoder = JSONDecoder()
+            do {
+                
+                //try to decode the data using the CoinData structure
+                let decodedData = try decoder.decode(CoinData.self, from: data)
+                
+                //Get the last property from the decoded data.
+                let lastPrice = decodedData.rate
+                print(lastPrice)
+                return lastPrice
+                
+            } catch {
+                
+                //Catch and print any errors.
+                print(error)
+                return nil
+            }
+        }
 }
