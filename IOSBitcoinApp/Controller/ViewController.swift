5b8503c8bb675f4149ea14ac336c2957e01b86d2
GkhKaya <gkhkaya00@gmail.com>
2023-08-23 18:03:28 +0300

    UIPickerView Functionality

--- IOSBitcoinApp/Controller/ViewController.swift
+++ IOSBitcoinApp/Controller/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+    
+    let coinManager = CoinManager()
+    
+    
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row:Int, forComponent component: Int) -> String? {
+        coinManager.currencyArray[row]
+    }
+        
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+    }
 }
-
---
+++ IOSBitcoinApp/Model/CoinManager.swift
@@ -0,0 +1,20 @@
+//
+//  CoinManager.swift
+//  IOSBitcoinApp
+//
+//  Created by Gokhan Kaya on 23.08.2023.
+//
+
+import Foundation
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "D47CF28C-F471-436C-818A-218C5E7843DD"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    func getCoinPrice(for currency:String){
+        
+    }
+    
+}
